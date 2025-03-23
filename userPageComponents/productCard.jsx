ed07d82945032d5e665f6145f7cd6de2a3bef534
AluculeseiAndrei <andrei.aluculesei@student.usv.ro>
2025-03-23 20:25:05 +0200

    Fix file name

--- userPageComponents/productCard.jsx
+++ userPageComponents/productCard.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import '../userPageCss/ProductCard.css';
+import '../userPageCss/productCard.css';
 import ProductDescriptionModal from '../modals/productDescription';
 
 const ProductCard = ({ productId, imageUrl, description, price, name }) => {
