72a1fcd932d8418e14adf1bc13cc87b915a8d402
AluculeseiAndrei <andrei.aluculesei@student.usv.ro>
2025-03-23 19:59:41 +0200

    Fix file name

--- userPage/viewRestarantMenu.jsx
+++ userPage/viewRestarantMenu.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState, useRef } from 'react';
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
-import '../userPageCss/ProductList.css';
+import '../userPageCss/productgitList.css';
 import ProductCard from '../userPageComponents/productCard';
 import Spinner from '../components/spinner';
 import UserNavBar from '../userPageComponents/navBarUserPage.jsx';
