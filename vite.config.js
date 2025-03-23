7cb596588f263c0ef7871e4029ec51337c3c5c4a
AluculeseiAndrei <andrei.aluculesei@student.usv.ro>
2025-03-23 21:58:39 +0200

    Routing fix

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// Export Vite configuration
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: "build" 
-  }
-  
+    outDir: "build",  // The build output folder
+  },
+  server: {
+    historyApiFallback: true,  // This ensures routes fallback to index.html, allowing React Router to handle them
+  },
 })
